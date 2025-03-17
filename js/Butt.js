c58da758dde8e9f34889f35d6194523d63e41aef
W1ndles <oswaldthetoon@gmail.com>
2025-03-17 12:37:26 +0300

    Add files via upload

---
+++ js/Butt.js
@@ -0,0 +1,11 @@
+$('#github').click(function() {
+    window.open('https://github.com/W1ndles', '_blank')
+});
+
+$('#games').click(function() {
+    window.open('games.html', '_self')
+});
+
+$('#about_me').click(function() {
+    window.open('about_me.html', '_self')
+});
