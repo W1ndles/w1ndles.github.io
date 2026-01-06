217c82dbacf1ee10ada5741ca86072b099239a90
W1ndles <oswaldthetoon@gmail.com>
2026-01-06 22:37:30 +0300

    Update Test_script.js

--- js/Test_script.js
+++ js/Test_script.js
@@ -1,23 +1,20 @@
-let name__np = {
-    "key1": ["Big Update", "Update"],
-    "key2": ["News"]
-};
 
+const np__create_key = document.querySelector('#name__np-key1');
+const np__create_key1 = document.querySelector('#name__np-key2');
+const np__create_key2 = document.querySelector('#name__np-key3');
 
-const np__create = document.querySelector('#name__np');
-const np__create__news = document.querySelector('#news__np');
+const np__create_name_key = document.createElement('h2');
+const np__create_name_key1 = document.createElement('h2');
+const np__create_name_key2 = document.createElement('h2');
 
-const np__news__create = document.createElement('h2');
-const np__name__create = document.createElement('h2');
+np__create_name_key.textContent = 'Крупное Обновление';
+np__create_key.appendChild(np__create_name_key);
 
+np__create_name_key1.textContent = 'Новость';
+np__create_key1.appendChild(np__create_name_key1);
 
-if (name__np.key1[0]) {
-    np__name__create.textContent = 'Крупное Обновление';
-    np__create.appendChild(np__name__create);
-};
-if (name__np.key2[0]) {
-    np__news__create.textContent = 'Новости';
-    np__create__news.appendChild(np__news__create);
-}
+np__create_name_key2.textContent = 'Обновление';
+np__create_key2.appendChild(np__create_name_key2);
+
 
 
