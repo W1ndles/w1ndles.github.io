56233d0a56bcd0ce783fb95a76bee38ef77d0033
W1ndles <oswaldthetoon@gmail.com>
2026-01-07 16:02:53 +0300

    Update Test_script.js

--- js/Test_script.js
+++ js/Test_script.js
@@ -7,14 +7,18 @@ const np__create_name_key = document.createElement('h2');
 const np__create_name_key1 = document.createElement('h2');
 const np__create_name_key2 = document.createElement('h2');
 
-np__create_name_key.textContent = 'Крупное Обновление';
-np__create_key.appendChild(np__create_name_key);
+if (np__create_key) {
+    np__create_name_key.textContent = 'Крупное Обновление';
+    np__create_key.appendChild(np__create_name_key);
+};
 
-np__create_name_key1.textContent = 'Новость';
-np__create_key1.appendChild(np__create_name_key1);
+if (np__create_key1) {
+    np__create_name_key1.textContent = 'Новость';
+    np__create_key1.appendChild(np__create_name_key1);
+};
 
-np__create_name_key2.textContent = 'Обновление';
-np__create_key2.appendChild(np__create_name_key2);
-
-
+if (np__create_key2) {
+    np__create_name_key2.textContent = 'Обнорвление';
+    np__create_key2.appendChild(np__create_name_key2);
+};
 
