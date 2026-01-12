b6b319e0bc3c677521606ec7474b8565c8403b1a
W1ndles <oswaldthetoon@gmail.com>
2026-01-12 18:10:50 +0300

    Update Test_script.js

--- js/Test_script.js
+++ js/Test_script.js
@@ -2,14 +2,18 @@
 const np__create_key = document.querySelector('#name__np-key1');
 const np__create_key1 = document.querySelector('#name__np-key2');
 const np__create_key2 = document.querySelector('#name__np-key3');
+const np__create_key3 = document.querySelector('#name__np-key4');
 
 const np__create_name_key = document.createElement('h2');
 const np__create_name_key1 = document.createElement('h2');
 const np__create_name_key2 = document.createElement('h2');
+const np__create_name_key3 = document.createElement('h2');
 
 if (np__create_key) {
     np__create_name_key.textContent = 'Крупное Обновление';
     np__create_key.appendChild(np__create_name_key);
+
+    np__create_key.querySelector('h2').style.color = "#007eff";
 };
 
 if (np__create_key1) {
@@ -22,4 +26,11 @@ if (np__create_key2) {
     np__create_key2.appendChild(np__create_name_key2);
 };
 
+function load__themes() {
+    $('.container__block_summary_alt').css({'color':'var(--white)'});
+};
 
+if (np__create_key3) {
+    np__create_name_key3.textContent = 'Небольшое Обновление';
+    np__create_key3.appendChild(np__create_name_key3);
+};
